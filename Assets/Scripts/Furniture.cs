4cd704959f1b2d0e3237627db5a99350cb11cc4a
Annika Brown <annbrown@chapman.edu>
2025-06-21 00:46:02 -0700

    modified furniture

--- Assets/Scripts/Furniture.cs
+++ Assets/Scripts/Furniture.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class Furniture : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject chairToCreate;
+    public GameObject tableToCreate;
     void Start()
     {
+        // create chairs
+        for (int i = 0; i < Parameters.numberOfChairs; i++)
+        {
+            GameObject newChair = Instantiate(chairToCreate, new Vector3(i - 58, -65.4299f, 64), Quaternion.identity);
+            newChair.tag = "Chair";
+            Layout.F.Add(newChair);
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // create tables
+        for (int i = 0; i < Parameters.numberOfTables; i++)
+        {
+            GameObject newTable = Instantiate(tableToCreate, new Vector3(i, 0.2f, 0), Quaternion.identity);
+            newTable.tag = "Table";
+            Layout.F.Add(newTable);
+        }
     }
 }
--- Assets/Scripts/Parameters.cs
+++ Assets/Scripts/Parameters.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public static class Parameters
 {
     public static int numberOfChairs = 4;
+    public static int numberOfTables = 1;
     public static int numberOfFurnitureItems = 4;
     public static int numberOfWalls = 4;
     public static int numberOfGroups = 1;
